9a4a09d4871c67bbae81e55427818f638843df80
joebrashear1 <joebrashear@knights.ucf.edu>
2021-11-21 20:02:40 -0500

    Added edamam API, getting macros from live camera feed now when a valid food is found

--- food_recognize.py
+++ food_recognize.py
@@ -5,7 +5,7 @@ Recognize food: fruit, vegetable
 import io
 import os
 from datetime import datetime
-
+import requests
 import cv2
 from google.cloud import vision_v1p3beta1 as vision
 
@@ -16,6 +16,9 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'client_key.json'
 SOURCE_PATH = "C:/Fruits/"
 
 FOOD_TYPE = 'Fruit'  # 'Vegetable'
+
+APP_ID = "f42907f5"
+APP_KEY = "c2b17669b4b9b1eff72c964c28c0d4b1"
 
 
 def load_food_name(food_type):
@@ -41,10 +44,10 @@ def recognize_food(img_path, list_foods):
     img = cv2.resize(img, (800, int((height * 800) / width)))
 
     # Save the image to temp file
-    cv2.imwrite(SOURCE_PATH + "output.jpg", img)
+    cv2.imwrite("output.jpg", img)
 
     # Create new img path for google vision
-    img_path = SOURCE_PATH + "output.jpg"
+    img_path = "output.jpg"
 
     # Create google vision client
     client = vision.ImageAnnotatorClient()
@@ -69,16 +72,37 @@ def recognize_food(img_path, list_foods):
             # print(desc, 'score: ', score)
 
             # Put text license plate number to image
-            cv2.putText(img, desc.upper() + " ???", (300, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 200), 2)
-            cv2.imshow('Recognize & Draw', img)
-            cv2.waitKey(0)
+            nutrition = requests.get("https://api.edamam.com/api/food-database/v2/parser?app_id=%s&app_key=%s&ingr=%s&nutrition-type=logging" %(APP_ID, APP_KEY, desc))
+            print("status code", nutrition.status_code)
+            nutrition_json = nutrition.json()
+            if nutrition.status_code == 200:
+                cv2.putText(img, str(nutrition_json["parsed"][0]["food"]["nutrients"]), (300, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 200), 2)
+                cv2.imshow('Recognize & Draw', img)
+                cv2.waitKey(0)
 
             # Get first fruit only
             break
 
     print('Total time: {}'.format(datetime.now() - start_time))
 
+cap = cv2.VideoCapture(0)
 
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    file = 'live.png'
+    cv2.imwrite( file,frame)
+    print('---------- Start FOOD Recognition --------')
+    list_foods = load_food_name(FOOD_TYPE)
+    print(list_foods)
+    recognize_food(file, list_foods)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    print('---------- End ----------')
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
 print('---------- Start FOOD Recognition --------')
 list_foods = load_food_name(FOOD_TYPE)
 print(list_foods)
